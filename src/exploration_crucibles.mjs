60b8cd64150389c7607c8e1fa98ffa7c6aa9cf58
Kevin Becker <kevinfjbecker@gmail.com>
2025-05-06 01:45:02 +0200

    exploration crucibles

---
+++ src/exploration_crucibles.mjs
@@ -0,0 +1,20 @@
+import * as fs from 'fs'
+import { getBlocks } from './util/blocks.mjs'
+import { readTextLines } from './util/textLines.mjs'
+
+const lines = readTextLines('./grimwild/txt/exploration_crucibles.txt')
+
+const blocks = getBlocks(lines)
+
+const crucible = [[[],[]],[[],[]],[[],[]]]
+
+const c = i => i % 3 // crucible index
+const p = i => Math.floor(i / 3) % 2 // part index
+
+for(let i = 0; i < blocks.length; i++)
+{
+    console.log(''+c(i)+p(i)+(Math.floor(i % 6)))
+    crucible[c(i)][p(i)].push(blocks[i])
+}
+
+fs.writeFileSync('./grimwild/json/exploration_crucibles.json', JSON.stringify(crucible, null, 4))
