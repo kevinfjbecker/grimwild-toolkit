3270565f6fcc515a6d5a6abfbf6b8f2950ce61aa
Kevin Becker <kevinfjbecker@gmail.com>
2025-06-10 15:47:12 +0200

    More crucibles

---
+++ src/more_crucibles.mjs
@@ -0,0 +1,24 @@
+import * as fs from 'fs'
+import { getBlocks } from './util/blocks.mjs'
+import { readTextLines } from './util/textLines.mjs'
+
+const lines = readTextLines('./grimwild/txt/more_crucibles.txt')
+
+const blocks = getBlocks(lines)
+
+const crucible = [[[],[]],[[],[]],[[],[]]]
+
+const c = i => i % 3 // crucible index
+const p = i => Math.floor(i / 3) % 2 // part index
+
+// todo: The crucibles are mixed up (fix)
+
+for(let i = 0; i < blocks.length; i++)
+{
+    // console.log(''+c(i)+p(i)+(Math.floor(i % 6)))
+    crucible[c(i)][p(i)].push(blocks[i])
+}
+
+fs.writeFileSync('./grimwild/json/more_crucibles.json', JSON.stringify(crucible, null, 4))
+
+// todo: add some sample text output
