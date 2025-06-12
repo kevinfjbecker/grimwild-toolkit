ba96dcc3efa1de2915b63a20902884ff88079a93
Kevin Becker <kevinfjbecker@gmail.com>
2025-06-12 02:22:55 +0200

    More Crucibles

--- src/more_crucibles.mjs
+++ src/more_crucibles.mjs
@@ -8,15 +8,20 @@ const blocks = getBlocks(lines)
 
 const crucible = [[[],[]],[[],[]],[[],[]]]
 
-const c = i => i % 3 // crucible index
-const p = i => Math.floor(i / 3) % 2 // part index
-
-// todo: The crucibles are mixed up (fix)
-
-for(let i = 0; i < blocks.length; i++)
+const c_top = i => 0 // crucible index
+const p_top = i => i % 2 // part index
+for(let i = 0; i < 12; i++)
 {
-    // console.log(''+c(i)+p(i)+(Math.floor(i % 6)))
-    crucible[c(i)][p(i)].push(blocks[i])
+    // console.log(''+c_top(i)+p_top(i)+(Math.floor(i % 6)))
+    crucible[c_top(i)][p_top(i)].push(blocks[i])
+}
+
+const c_bot = i => i % 2 + 1 // crucible index
+const p_bot = i => Math.floor(i / 2) % 2 // part index
+for(let i = 12; i < blocks.length; i++)
+{
+    // console.log(''+c_bot(i)+p_bot(i)+(Math.floor(i % 6)))
+    crucible[c_bot(i)][p_bot(i)].push(blocks[i])
 }
 
 fs.writeFileSync('./grimwild/json/more_crucibles.json', JSON.stringify(crucible, null, 4))
